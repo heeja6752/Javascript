7b96287a48cf4901d500ef9c3704936e108a8609
Xiao <43363703+heeja6752@users.noreply.github.com>
2022-02-18 15:00:35 +0900

    Create 3.대화상자.js

---
+++ 3.대화상자.js
@@ -0,0 +1,18 @@
+const name = prompt("이름을 입력하세요.");
+alert("환영합니다,"+ name + "님");
+
+- - - - -
+const name = prompt("이름을 입력하세요.");
+alert(`안녕하세요, ${name}님. 환영합니다`);
+
+- - - - -
+const name = prompt("이름을 입력하세요.");
+console.log(name)
+
+- - - - -
+const name = prompt("예약일을 입력해주세요.","2020-10-");
+
+- - - - -
+const isAdult = confirm("당신은 성인 입니까?")
+
+console.log(isAdult)
