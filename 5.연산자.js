566ad6c5dbeb8dec5502d9074756845bd2b53eae
Xiao <43363703+heeja6752@users.noreply.github.com>
2022-02-21 17:11:42 +0900

    Create 5.연산자.js

---
+++ 5.연산자.js
@@ -0,0 +1,27 @@
+//연산자 줄여서 쓰기
+
+let num = 10;
+//num = num + 5;
+num +=5;
+// *=, -=, /=, %=
+
+console.log(num);
+
+
+- - - - -
+  
+// 증가 연산자, 감소 연산자
+
+let num = 10;
+num++;
+
+console.log(num);
+
+- - - 
+  
+let num = 10;
+let result = ++num; //num++; 
+
+console.log(result);
+  
+  
