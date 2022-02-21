c67a703c466b8b821c681b0cc9cfdec079525dc8
Xiao <43363703+heeja6752@users.noreply.github.com>
2022-02-21 16:10:42 +0900

    Create 4.형변환.js

---
+++ 4.형변환.js
@@ -0,0 +1,27 @@
+//const mathScore = prompt("수학 몇점?");
+//const engScore = prompt("영어 몇점?");
+const mathScore = 90;
+const engScore = 80;
+const result = (mathScore + engScore) / 2 ;
+
+console.log(result)
+
+
+- - - - -
+  
+console.log(
+String(3),
+String(true),
+String(false),
+String(null),
+String(undefined)
+)
+
+
+- - - - -
+console.log(
+  Number("1234"),
+  Number("1234asdf"),
+  Number(true),
+  Number(false)
+)
