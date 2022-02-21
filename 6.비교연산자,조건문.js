d3cbf40ed93102e7b1d9c5f80c0f9e56a8d4d4a8
Xiao <43363703+heeja6752@users.noreply.github.com>
2022-02-21 17:35:01 +0900

    Create 6.비교연산자,조건문.js

---
+++ 6.비교연산자,조건문.js
@@ -0,0 +1,32 @@
+console.log(10>5);
+console.log(10 == 5); //동등 연산자
+console.log(10 != 5);
+
+//동등 연산자
+
+const a= 1;
+const b = "1";
+
+console.log(a === b); //일치 연산자
+
+
+- - - - -
+  
+// if, else, else if
+//추가 요구사항 : 
+//19살이면 수능 잘치세요 라는 문구를 보여주세요
+//age === 19
+
+const age = ;
+
+if(age>19){
+  console.log('환영합니다');
+}
+else if(age === 19) {
+  console.log('수능 잘치세요')
+}
+else{
+  console.log('안녕히 가세요');
+}
+
+console.log('-----------------------')
