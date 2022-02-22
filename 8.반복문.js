d9ba94c339d7064e1174bb3023c3497d68b536fa
Xiao <43363703+heeja6752@users.noreply.github.com>
2022-02-22 16:58:04 +0900

    Create 8.반복문.js

---
+++ 8.반복문.js
@@ -0,0 +1,17 @@
+//1부터 10까지 로그
+
+for(let i=0; i<10; i++){console.log(i+1)  
+}
+
+- - -
+
+for(let i=1; i<11; i++){console.log(i)  
+}
+
+- - - 
+  
+for(let i=1; i<=10; i++){console.log(i)  
+}
+
+- - - - - 
+  
