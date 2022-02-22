119d56cd20d6569a2558b16196448495da660650
Xiao <43363703+heeja6752@users.noreply.github.com>
2022-02-22 17:20:00 +0900

    Update 8.반복문.js

--- 8.반복문.js
+++ 8.반복문.js
@@ -14,4 +14,40 @@ for(let i=1; i<=10; i++){console.log(i)
 }
 
 - - - - - 
+
+//while
+
+let i = 0;
+
+while(i<10){
+  console.log(i);
+  i++;
+}
+
+
+- - - - -
+  
+//break
+
+while(true){
+  let answer = confirm('계속 할까요?') 
+  if(!answer){
+    break;
+  }
+}
+
+- - - - -
+  
+//continue
+//짝수만
+
+for(let i=0; i<10; i++){
+  if(i%2){
+    continue;
+  }
+  console.log(i)
+}
+
+
+
   
