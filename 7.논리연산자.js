dc3af01aaa3c94f895992b5917bec602a1bd74b2
Xiao <43363703+heeja6752@users.noreply.github.com>
2022-02-22 16:35:16 +0900

    Update 7.논리연산자.js

--- 7.논리연산자.js
+++ 7.논리연산자.js
@@ -35,3 +35,20 @@ if(!isAge){
 }
 
 console.log('---------')
+
+- - - - -
+  
+//우선순위
+// 남자이고, 이름이 Mike 이거나 성인이면 통과
+
+const gender = 'M'
+const name = 'Jane';
+const isAdult = true;
+
+//if(gender === 'M' && name === 'Mike' || isAdult){
+if(gender === 'M' && (name === 'Mike' || isAdult)){
+  console.log('통과')
+}
+else {
+  console.log('돌아가.')
+}
