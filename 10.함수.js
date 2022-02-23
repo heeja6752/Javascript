06fb4ed6bb9cacf32acf8340a6e46277b985bfb1
Xiao <43363703+heeja6752@users.noreply.github.com>
2022-02-23 18:12:14 +0900

    Update 10.함수.js

--- 10.함수.js
+++ 10.함수.js
@@ -106,3 +106,28 @@ function sayHello(name = 'friend'){
 
 sayHello();
 sayHello('Jane');
+
+- - - - -
+  
+// return 으로 값 변환
+
+
+function add(num1, num2){
+  return num1 + num2;
+}
+
+const result = add(2,3);
+console.log(result)
+
+
+- - -
+  
+
+function showError(){
+  alert('에러가 발생했습니다.');
+  return;
+  alert('이 코드는 절대 실행되지 않습니다.')
+}
+const result = showError();
+console.log(result);
+  
