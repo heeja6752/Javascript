d59a291978cfa75a03f48ae53d8f5b4958be26d5
Xiao <43363703+heeja6752@users.noreply.github.com>
2022-02-26 16:17:04 +0900

    Update 12.객체.js

--- 12.객체.js
+++ 12.객체.js
@@ -57,3 +57,41 @@ console.log(Mike);
 
 console.log("age" in Mike);
 console.log("birthday" in Mike);
+
+
+- - - - -
+  
+
+//객체 in
+
+function isAdult(user){
+  if(!('age' in user) ||
+    user.age < 20) {
+    return false;
+  }
+  return true;
+}
+
+const Mike = {
+  name: "Mike",
+  age: 30
+};
+
+const Jane = {
+  name: "Jane"
+};
+
+console.log(isAdult(Jane))
+
+- - -
+  
+// 객체 for ... in
+
+const Mike = {
+  name: "Mike",
+  age : 30
+};
+
+for (x in Mike){
+  console.log(Mike[x]) //Mike['age']
+}
