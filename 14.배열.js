51cf4b7523a99da3c76451b4ac030fea1db6e2e4
Xiao <43363703+heeja6752@users.noreply.github.com>
2022-03-01 19:14:41 +0900

    Update 14.배열.js

--- 14.배열.js
+++ 14.배열.js
@@ -1,1 +1,66 @@
 
+//array
+
+let days = ["mon", "tue", "wed"];
+
+days[1] = '화요일'
+
+console.log(days)
+
+
+- - -
+  
+
+let days = ["mon", "tue", "wed"];
+
+days[1] = '화요일'
+
+console.log(days.length)
+
+- - -
+  
+  
+let days = ["mon", "tue", "wed"];
+
+days.push('thu')
+
+console.log(days);
+
+
+- - -
+  
+
+let days = ["mon", "tue", "wed"];
+
+days.push("thu")
+
+days.unshift("sun")
+
+console.log(days);
+
+
+- - - - -
+  
+//for문
+  
+let days = ["mon", "tue", "wed"];
+
+days.push("thu")
+
+days.unshift("sun")
+
+for(let index=0;index<days.length;index++){
+  console.log(days[index]);
+}
+
+- - -
+
+let days = ["mon", "tue", "wed"];
+
+days.push("thu")
+
+days.unshift("sun")
+
+for(let day of days) {
+  console.log(day);
+}
